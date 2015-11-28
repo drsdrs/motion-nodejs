4b66eb9ad18920ad1d2ddf4f1d3875c88818781b
drsdrs <ae@dnilabs.com>
2015-11-28 21:59:25 +0100

    rework everything

--- app/app.js
+++ app/app.js
@@ -1,12 +1,26 @@
-
-var images
-, pos, intervalId, startAt, endAt
-, startAtEl, playtStopEl, imgEl, imgPosEl
-, play= true
-, imgLoading= false
+var images, pos, intervalId, startAt, endAt
+, startAtEl, playtStopEl, imgEl, imgPosEl, rmImagesEl, rmAllImagesEl, makeGifEl
+, errorReported = false
+, mousedown = false
+, play = true
+, imgLoading = false
 , firstImgDataLoad = true
 , timeLoadingImg = Date.now()
-;
+, motionOptions = [
+  "width",
+  "height",
+  "rotate",
+  "pre_capture",
+  "post_capture",
+  "framerate",
+  "threshold",
+  "threshold_tune",
+  "noise_level",
+  "noise_tune",
+  "quality",
+  "text_right",
+  "text_left"
+];
 
 window.onload = function(){
   images = [];
@@ -15,101 +29,303 @@ window.onload = function(){
   spdSliderEl = document.getElementById("spdSlider");
   startAtEl = document.getElementById("startAt");
   endAtEl = document.getElementById("endAt");
+  videoWrapEl = document.getElementById("videoWrap");
   playStopEl = document.getElementById("playStop");
   imgPosEl = document.getElementById("imgPos");
   imgPosSliderEl = document.getElementById("imgPosSlider");
+  rmImagesEl = document.getElementById("rmImages");
+  rmAllImagesEl = document.getElementById("rmAllImages");
+  motionOptionsEl = document.getElementById("motionOptions");
+  motionOptionInputEl = document.getElementById("motionOptionInput");
+  motionOptionInfoEl = document.getElementById("motionOptionInfo");
+  startStopMotionEl = document.getElementById("startStopMotion");
+  toggleStreamEl = document.getElementById("toggleStream");
+  liveStreamEl = document.getElementById("liveStream");
+  writeMotionConfigEl = document.getElementById("writeMotionConfig");
+  makeMovieSelEl = document.getElementById("makeMovieSel");
+  makeMovieAllEl = document.getElementById("makeMovieAll");
 
-  chStartAt = function(){
-    pos = startAt = startAtEl.value;
-    if(endAtEl.value<=startAt){ endAtEl.value = startAt; console.log("endAtEl small", endAtEl.value+">"+startAt)}
-    startAtEl.previousElementSibling.innerHTML = "start at "+startAt;
+  function toggleStream(e){
+    liveStreamEl.style.display = "block";
+    closeBtn = document.createElement("button");
+    closeBtn.innerHTML = closeBtn.innerText ="X";
+    closeBtn.addEventListener("click", function(e){
+      liveStreamEl.innerHTML = "";
+      liveStreamEl.style.display = "none";
+    });
+    img = document.createElement("img");
+    img.src = "http://127.0.0.1:3600/";
+    liveStreamEl.appendChild(img);
+    liveStreamEl.appendChild(closeBtn);
   }
 
-  chEndAt = function(){ 
-    endAt = endAtEl.value;
-    if(startAtEl.value>=endAt){ startAtEl.value = endAt; console.log("startAtEl big", startAtEl.value+">"+endAt) }
-    endAtEl.previousElementSibling.innerHTML = "end at "+endAt;
+  function makeMovieAll(){
+    pre_toggleDownloadMovieWindow()
+    post("makeMovieAll", "fps="+spdSliderEl.value, function(res){
+      toggleDownloadMovieWindow();
+      console.log("makeMovieAll done...");
+    });
+  }
+
+  function makeMovieSel(){
+    pre_toggleDownloadMovieWindow()
+    post("makeMovieSel", "start="+startAt+"&end="+endAt+"&fps="+spdSliderEl.value, function(res){
+      toggleDownloadMovieWindow();
+      console.log("makeMovieSel done...");
+    });
+  }
+
+  function writeMotionConfig(){
+    post("writeMotionConfig");
+  }
+
+  function pre_toggleDownloadMovieWindow(){
+    liveStreamEl.style.display = "block";
+    loadingEl = document.createElement("div");
+    loadingEl.className = "loading";
+    liveStreamEl.appendChild(loadingEl);
+  }
+
+  function toggleDownloadMovieWindow(){
+    closeBtn = document.createElement("button");
+    closeBtn.innerHTML = closeBtn.innerText ="X";
+    closeBtn.addEventListener("click", function(e){
+      liveStreamEl.innerHTML = "";
+      liveStreamEl.style.display = "none";
+    });
+    downloadBtn = document.createElement("a");
+    downloadBtn.download = "movie.mp4";
+    downloadBtn.href = "/movie.mp4";
+    downloadBtn.innerHTML = downloadBtn.innerText ="Download";
+    downloadBtn.addEventListener("click", function(e){
+      // download movie.mp4
+    });
+    video = document.createElement("video");
+    video.src = "movie.mp4";
+    video.loop = true;
+    video.autoplay = true;
+    video.controls = true;
+    liveStreamEl.appendChild(video);
+    liveStreamEl.appendChild(closeBtn);
+    liveStreamEl.appendChild(downloadBtn);
+  }
+
+  function startStopMotion(e){
+    e.target.innerHTML = "Loading..."
+    post("startStopMotion", "", function(res){
+      if(res=="running") { btnText = "Stop motion"; }
+      else if(res=="stopped") { btnText = "Start motion"; }
+      else { btnText = "ERROR"; }
+      e.target.innerHTML = btnText;
+    });
+  }
+
+  function initMotionOptions(){
+    motionOptions.forEach(function(v,i) {
+      setTimeout(function(){ // add delay because the order is mixed sometimes if req isnt fast enough
+        post('getMotionConfig', "k="+v, function(res){
+          if(res==="noConnectionError"&&errorReported===false){
+            errorReported = true;
+            setTimeout(function(){
+              startStopMotionEl.click();
+              location.reload();
+            }, 1000);
+          }
+          var select = document.createElement("option");
+          var optionText = v+" = "+res.split(v+" = ")[1].split(" ")[0];
+          console.log(optionText);
+          select.value = v;
+          select.innerText = select.innerHTML = optionText;
+          motionOptionsEl.appendChild(select);
+        })
+      }, 100);
+    });
+  }
+
+  function changeMotionOption(e){
+    post('getMotionConfig', "k="+e.target.value, function(res){
+      ghostEl = document.createElement("div")
+      ghostEl.innerHTML = res;
+      info = ghostEl.getElementsByTagName("i")[0];
+      data = ghostEl.getElementsByTagName("ul")[0].innerHTML;
+      helpLink = ghostEl.getElementsByTagName("a")[1];
+      motionOptionInfoEl.innerHTML = "";
+      motionOptionInfoEl.appendChild(info);
+      motionOptionInfoEl.appendChild(helpLink);
+      motionOptionInput.value = data.split(" = ")[1].split(" ")[0];
+    })
+  }
+
+  chStartAt = function(){
+    startAt = parseInt(startAtEl.value);
+    endAt = parseInt(endAtEl.value);
+    console.log(startAtEl.value,  endAtEl.value);
+    if(startAt>endAt){
+      endAtEl.value = startAt;
+      console.log("endAtEl small", endAtEl.value+"<="+startAt);
+    }
+    startAtEl.previousElementSibling.innerHTML = "startPos: "+startAt;
+  }
+
+  chEndAt = function(){
+    startAt = parseInt(startAtEl.value);
+    endAt = parseInt(endAtEl.value);
+    console.log(startAtEl.value, endAtEl.value);
+    if(startAt>endAt){
+      startAtEl.value = endAt;
+      console.log("startAtEl big", startAtEl.value+">"+endAt);
+    }
+    endAtEl.previousElementSibling.innerHTML = "endPos: "+endAt;
   }
 
   spdSliderEl.addEventListener("change", function(e){
-    if(play){ startAnimation(e.target.value); }
-  }); 
-
-
-  startAtEl.addEventListener("change", chStartAt);
-  endAtEl.addEventListener("change", chEndAt);
-
-  playStopEl.addEventListener("click", function(e){
-    play = !play;
-    imgLoading = false;
-    if(play){ startAnimation(spdSliderEl.value); } else { stopAnimation(); }
+    this.previousElementSibling.innerHTML = "Fps: "+this.value;
+    if(play){ startAnimation(1000/e.target.value); }
   });
 
-  imgPosSliderEl.addEventListener("change", function(e){
-    chImage
-    pos = e.target.value;
-    return
-  }); 
 
-  // INIT
-  loadImgData(8000);
-  startAnimation(spdSliderEl.value);
-  endAtEl.value = endAtEl.max = images.length;
-  chEndAt();
-}
+  function setMotionConfig(e){
+    if(e.target.value.length>0){
+      var len = e.target.previousElementSibling.childElementCount;
+      while(len--){
+        el = e.target.previousElementSibling[len];
+        console.log(el);
+        if(el.value===motionOptionsEl.value){
+          el.innerHTML = motionOptionsEl.value+" = "+e.target.value
+        }
+      }
+      post('setMotionConfig', "k="+motionOptionsEl.value+"&v="+e.target.value)
+    }
+  }
 
-loadImgData = function(refreshRate){
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET","/images", true);
-  xmlhttp.onreadystatechange=function(){
-    if (xmlhttp.readyState==4 && xmlhttp.status==200){
-      images = JSON.parse(xmlhttp.responseText);
-      initSliders();
-      if(firstImgDataLoad===true){
-        endAtEl.max = images.length;
-        firstImgDataLoad = false;
-        chEndAt();
+
+  function loadImgData(refreshRate){
+    loadImageCallback = function(result){
+      if(result) {
+        data = JSON.parse(result);
+        images = data;
+        initSliders();
+        if(firstImgDataLoad===true){
+          endAtEl.max = images.length-1;
+          endAtEl.value = images.length-1;
+          firstImgDataLoad = false;
+          chEndAt();
+        }
       }
       setTimeout(function(){ loadImgData(refreshRate); }, refreshRate);
     }
-   }
-   xmlhttp.send();
-}
-
-initSliders = function(){
-  len = images.length-1;
-  if(len>0){ startAtEl.max= len }else{ startAtEl.max =0 };
-  startAtEl.previousElementSibling.innerHTML = "start at "+startAtEl.value;
-  startAt = startAtEl.value;
-  endAtEl.max = len;
-  imgPosSliderEl.max = len
-}
-
-chImage = function(position) {
-  pos = position || pos;
-  len = images.length;
-  src = images[pos]||null;
-  if((pos >= endAt) && play) { pos = startAt;};
-  if(src===null || imgLoading===true){
-    console.log("too fast || no images : "+src+" Pos:"+pos+" IMG onload"+ imgLoading);
-    return imgLoading = false;
+    post("/getImages", "imageLength="+images.length, loadImageCallback)
   }
-  imgLoading = true;
-  // timeLoadingImg = Date.now()
-  imgEl.src = src;
-  imgEl.onload = function(){
-    imgPosEl.innerHTML = "pos :"+pos;
-    imgPosSliderEl.value = pos;
-    //console.log("loadTime "+(Date.now()-timeLoadingImg)+"ms");
-    return imgLoading = false;
-  }
-  return pos++;
-}
 
-stopAnimation = function(){ clearInterval(intervalId); }
-startAnimation = function(interval){
-  clearInterval(intervalId);
-  intervalId = setInterval(function(){
-    chImage();
-  }, interval);
+  function rmImages(){
+    if (!confirm('Delete selected images?')) { return false; }
+    post("/rmImages", "start="+startAt+"&end="+endAt);
+    location.reload();
+  }
+
+  function rmAllImages(){
+    if (!confirm('Delete all images?')) { return false; }
+    post("/rmAllImages");
+    location.reload();
+  }
+
+   function initSliders(){
+    len = images.length-1;
+    if(len>0){ startAtEl.max= len }else{ startAtEl.max =0 };
+    startAtEl.previousElementSibling.innerHTML = "start at "+startAtEl.value;
+    startAt = startAtEl.value;
+    endAtEl.max = len;
+    imgPosSliderEl.max = len
+  }
+
+  function slideImages(e){
+    if (pos != e.target.value){
+      console.log("slide");
+      pos = e.target.value;
+      chImage(pos);
+    }
+    return false;
+  }
+
+  function chImage(position) {
+    pos = position || pos;
+    len = images.length;
+    if (endAt-startAt<1&&intervalId>0) return console.log("to less images...")
+    src = images[pos]||null;
+    if((pos >= endAt) && play) { pos = startAt;};
+    if(src===null || imgLoading===true){
+      return imgLoading = false;
+    }
+    imgLoading = true;
+    imgEl.src = src;
+    imgEl.onload = function(){
+      imgPosEl.innerHTML = "pos :"+pos;
+      if (intervalId>0) imgPosSliderEl.value = pos-1;
+      return imgLoading = false;
+    }
+    return pos++;
+  }
+
+  function post(url, sendData, responseCallback) {
+    var xmlhttpRequest = new XMLHttpRequest();
+    xmlhttpRequest.open("POST", url, true);
+    xmlhttpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttpRequest.onreadystatechange = function(){
+      if (xmlhttpRequest.readyState==4 && xmlhttpRequest.status==200 && typeof(responseCallback)==="function" ){
+        responseCallback((xmlhttpRequest.responseText));
+      }
+    }
+    return xmlhttpRequest.send(sendData||'');
+  }
+
+  function get(url, sendData) {
+    var xmlhttpRequest = new XMLHttpRequest()
+    xmlhttpRequest.open("GET", url, true);
+    return xmlhttpRequest.send();
+  }
+
+  function stopAnimation(){
+    clearInterval(intervalId);
+    intervalId = -1;
+  }
+
+  function startAnimation(interval){
+    clearInterval(intervalId);
+    intervalId = setInterval(chImage, interval);
+  }
+
+  function startStop(e){
+    e.stopPropagation()
+    play = !play;
+    if(play){ playStopEl.innerHTML = "STOP" }
+    else { playStopEl.innerHTML = "PLAY" }
+    imgLoading = false;
+    if(play){ startAnimation(1000/spdSliderEl.value); }
+    else { stopAnimation(); }
+    return false
+  };
+
+  startAtEl.addEventListener("change", chStartAt);
+  endAtEl.addEventListener("change", chEndAt);
+  imgPosSliderEl.addEventListener("mousemove", function(e){ if(mousedown==true){ slideImages(e); } });
+  imgPosSliderEl.addEventListener("keyup", slideImages);
+  imgPosSliderEl.addEventListener("mousedown", function(e){ stopAnimation(); mousedown = true; slideImages(e); } );
+  imgPosSliderEl.addEventListener("mouseup", function(e){ startAnimation(1000/spdSliderEl.value); mousedown = false; slideImages(e);  });
+  rmImagesEl.addEventListener("click", rmImages);
+  rmAllImagesEl.addEventListener("click", rmAllImages);
+  motionOptionsEl.addEventListener("change", changeMotionOption)
+  motionOptionInputEl.addEventListener("blur", setMotionConfig)
+  writeMotionConfigEl.addEventListener("click", writeMotionConfig)
+  startStopMotionEl.addEventListener("click", startStopMotion)
+  toggleStreamEl.addEventListener("click", toggleStream)
+  makeMovieAllEl.addEventListener("click", makeMovieAll)
+  makeMovieSelEl.addEventListener("click", makeMovieSel)
+  playStopEl.addEventListener("click", startStop);
+  videoWrapEl.addEventListener("click", startStop);
+
+  initMotionOptions();
+  loadImgData(8000);
+  startAnimation(1000/spdSliderEl.value);
+  endAtEl.value = endAtEl.max = images.length;
+  chEndAt();
 }
--- s.coffee
+++ s.coffee
@@ -1,27 +1,99 @@
+c= console;c.l= c.log
 express = require('express')
 fs = require("fs")
-app = express()
-# compression = require('compression')
 serveStatic = require('serve-static')
+bodyParser = require("body-parser")
+querystring = require('querystring')
+request = require('request')
+exec = require('child_process').exec
+spawn = require('child_process').spawn
 
-c= console;c.l= c.log
+app = express()
+body_parser = bodyParser.urlencoded({ extended: false })
+imagePath = __dirname+"/images/"
+moviePath = __dirname+"/movies/"
+staticRootDir = express.static __dirname + '/app'
+imageList = []
 
-# app.use compression()
-app.use '/', express.static __dirname + '/app'
-app.use serveStatic '/home/images', { 'maxAge': 68000000, 'index':false, 'dotfiles':'ignore'  }
 
+app.use '/', staticRootDir
 
-app.get '/images', (req, res)->
-  fs.readdir "/home/images", (err, files)->
-    if err then return throw err
-    c.l "images :"+files.length
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end JSON.stringify(files)
+app.use serveStatic imagePath, { 'maxAge': 68000000, 'index':false, 'dotfiles':'ignore' }
+app.use serveStatic moviePath, { 'maxAge': 68000000, 'index':false, 'dotfiles':'ignore' }
 
+motion =
+  running: false
+  process: null
+  start: (cb)->
+    console.log "Start motion"
+    motion.process = spawn "motion", ["-c", "motion.conf"]
+    motion.process.stderr.once 'data', -> cb()
+    motion.running = true
+  stop: (cb)->
+    console.log "Kill all motion"
+    exec "killall motion", cb
+    motion.running = false
+    motion.process = null
+  config:
+    set: (k,v, cb)-> post("http://127.0.0.1:3900/0/config/set?"+k+"="+v, cb)
+    get: (k, cb)-> post("http://127.0.0.1:3900/0/config/get?query="+k, cb)
+    write: (cb)->
+      c.l "CONFIG WRITEN"
+      post "http://127.0.0.1:3900/0/config/write", ->
+        post "http://127.0.0.1:3900/0/config/writeyes", ->
+          post "http://127.0.0.1:3900/0/action/restart", cb() if cb?
+
+convertImg2Mov = (data, cb)->
+  captureRunning = false
+  if data.start? && data.end?
+    imagesToAnimate = imageList.slice data.start, data.end
+    imagesString = imagesToAnimate.join " "
+    command = 'cat '+imagesString+'| avconv -f image2pipe -r '+data.fps+' -i - -codec:v libx264 -b:v 256k -an movies/'+data.name+'.mp4'
+  else
+    command = 'cat images/*.jpg | avconv -f image2pipe -r '+data.fps+' -i - -codec:v libx264 -b:v 256k -an movies/'+data.name+'.mp4'
+  exec command, (error, stdout, stderr)->
+    if error? then console.log('convertImg2Mov error: '+error)
+    else cb() if cb?
+
+post = (url, cb)->
+  request.get { url:url }, (err, httpResponse, body)->
+    if err then console.log "postEr:"+"url:",url
+    else
+      if cb? then cb err, body
+
+getImages = (files)->
+  addPrefix = (files)->
+    files.forEach (v,i)->
+      files[i] = imagePath+v
+    imageList = files
+  if files? then addPrefix files
+  else
+    fs.readdir imagePath, (err, files)->addPrefix files
+
+getImages()
+
+app.post '/makeMovieAll', body_parser, (req, res)->
+  exec("rm -f movies/*")
+  movieParams =
+    fps:parseFloat(req.body.fps)
+    name:"movie"
+  convertImg2Mov movieParams, ()->
+    res.end "done!!!"
+
+app.post '/makeMovieSel', body_parser, (req, res)->
+  c.l "MAKEmOVIE SEL", req.body
+  exec("rm -f movies/*")
+  movieParams =
+    fps:parseFloat(req.body.fps)
+    name:"movie"
+    start: parseInt(req.body.start)
+    end: parseInt(req.body.end)
+  convertImg2Mov movieParams, ()->
+    res.end "done!!!"
 
 app.get '/download', (req, res)->
   spawn = require("child_process").spawn
-  zip = spawn("zip", ["-r", "-", ".", "*"], cwd: "/home/images")
+  zip = spawn("zip", ["-r", "-", ".", "*"], cwd: imagePath)
   res.contentType "application/zip"
   zip.stdout.on "data", (data) -> res.write data
   zip.stderr.on 'data', (data) -> console.log 'zip stderr: '+data
@@ -34,13 +106,26 @@ app.get '/download', (req, res)->
       console.log "zip done"
       res.end()
 
-app.get '/rm', (req, res)->
+app.post '/getImages', body_parser,(req, res)->
+  clientImageCount =  req.body.imageLength
+  fs.readdir imagePath, (err, files)->
+    if err then return throw err
+    c.l clientImageCount,files.length
+    if clientImageCount!=files.length
+      getImages()
+      c.l "Get images :"+files.length
+      res.writeHead(200, {"Content-Type": "application/json"});
+      res.end JSON.stringify(files)
+    else
+      console.log "get images NONONO!"
+      res.end()
+
+app.post '/rmAllImages', (req, res)->
   spawn = require("child_process").spawn
-  rm = spawn("rm", ["/home/images/", "-r"])
+  rm = spawn("rm", [imagePath, "-r"])
   rm.stderr.on 'data', (data) -> console.log 'rm stderr: '+data
   rm.on "exit", (code) ->
-    mkdir = spawn("mkdir", ["/home/images/"])
-
+    mkdir = spawn("mkdir", [imagePath])
     if code isnt 0
       res.statusCode = 500
       console.log "rm exited with code " + code
@@ -49,8 +134,47 @@ app.get '/rm', (req, res)->
       console.log "rm done"
       res.end()
 
+app.post '/rmImages', body_parser, (req, res)-> # get imagelist
+  spawn = require("child_process").spawn
+  imagesToDel = imageList.slice parseInt(req.body.start), parseInt(req.body.end)
+  rm = spawn("rm", imagesToDel)
+  rm.stderr.on 'data', (data) -> console.log 'rm stderr: '+data
+  rm.stdout.on 'data', (data) -> console.log 'rm stdout: '+data
+  rm.on "exit", (code) ->
+    res.end("done")
 
 
 
+app.post '/getMotionConfig', body_parser,(req, res)->
+  motion.config.get req.body.k, (err, result)->
+    if err?
+      console.log "getMotionConfigError"
+      res.end "noConnectionError"
+    else res.end String result
 
-app.listen 9000
+app.post '/setMotionConfig', body_parser,(req, res)->
+  motion.config.set req.body.k, req.body.v, (err, result)->
+    if err? then console.log "SetMotionConfig Error"
+    res.end String result
+
+app.post '/writeMotionConfig', (req, res)-> motion.config.write()
+
+app.post '/startStopMotion', (req, res)->
+  if motion.running==true
+    motion.running = "floating"
+    motion.stop (result)->
+      motion.running = false
+      res.end "stopped"
+  else if motion.running==false
+    motion.running = "floating"
+    motion.start (result)->
+      motion.running = true
+      res.end "running"
+  else
+    res.end "progress"
+
+#   INIT
+ motion.stop ->
+   motion.start ->
+      app.listen 9000
+      console.log "Server listen on http://localhost:9000"
